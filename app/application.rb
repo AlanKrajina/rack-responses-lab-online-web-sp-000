2efa6d1ef87847c211b9cc008f8adc53738d1ba6
Alan Krajina <alankrajina88@gmail.com>
2019-07-03 14:29:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-    if Time.now.hour.between?(0, 11)
+    if Time.now.hour < 12
       resp.write "Good Morning!"
     elsif Time.now.hour.between?(12, 17)
       resp.write "Good Afternoon."
